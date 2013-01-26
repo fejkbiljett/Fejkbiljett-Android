42a90829f27e7248252a757d0f3e991571fe10eb
Fejkbiljett <fejkbiljett@tormail.org>
2013-01-26 19:00:12 +0100

    Removed News pager and Internet permission.
    Because the old website is dead.

--- src/com/fejkbiljett/android/Utils.java
+++ src/com/fejkbiljett/android/Utils.java
@@ -1,55 +1,14 @@
 package com.fejkbiljett.android;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.preference.PreferenceManager;
 
 public class Utils {
 	public static String[] gAlphabet = { "A", "B", "C", "D", "E", "F", "G",
 			"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
 			"U", "V", "W", "X", "Y", "Z" };
-
-	public static String getNews(Activity context) {
-		try {
-			String version = getVersion(context);
-			String sId = PreferenceManager.getDefaultSharedPreferences(context)
-					.getString("uniqid", "NaN");
-			String url = "http://smsticket.fejkbiljett.com/" + version
-					+ "/getnews?id=" + sId;
-
-			HttpURLConnection conn = (HttpURLConnection) new URL(url)
-					.openConnection();
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					conn.getInputStream()));
-
-			char[] buf = new char[1024];
-			int read = br.read(buf, 0, 1024);
-
-			if (read < 0) {
-				return null;
-			}
-
-			String str = new String(buf);
-			return str.substring(0, read);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
 
 	public static String generateRandomString() {
 		return Utils.generateRandomString(9, false);
--- src/com/fejkbiljett/android/generators/TicketGeneratorActivity.java
+++ src/com/fejkbiljett/android/generators/TicketGeneratorActivity.java
@@ -4,12 +4,9 @@ import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.TextView;
@@ -98,142 +95,6 @@ public abstract class TicketGeneratorActivity extends SherlockFragmentActivity
 
 		ActionBar ab = getSupportActionBar();
 		ab.setTitle(mCityName);
-
-		{
-			SharedPreferences prefs = PreferenceManager
-					.getDefaultSharedPreferences(this);
-			Editor editor = prefs.edit();
-			long time = System.currentTimeMillis() / 1000;
-
-			if (time - 600 > prefs.getLong("news_updated", 0)) {
-				editor.putString("news_text", "-");
-				editor.putString("news_url", "-");
-				editor.putLong("news_updated", 0);
-				editor.commit();
-			}
-		}
-
-		final TextView text_news = (TextView) findViewById(R.id.text_news);
-
-		final Handler handler = new Handler() {
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-				case 1:
-					Utils.showAlert(
-							TicketGeneratorActivity.this,
-							"Obsolete",
-							"Denna applikation börjar bli gammal och kommer snart sluta fungera. "
-									+ "Besök http://fejkbiljett.com för senaste versionen.");
-					break;
-				case 2:
-					new AlertDialog.Builder(TicketGeneratorActivity.this)
-							.setTitle("Out of Date")
-							.setMessage(
-									"Denna applikation är gammal och fungerar inte längre. "
-											+ "Besök http://fejkbiljett.com för att uppdatera!")
-							.setNeutralButton("OK!",
-									new DialogInterface.OnClickListener() {
-										public void onClick(
-												DialogInterface dialog,
-												int which) {
-											TicketGeneratorActivity.this
-													.finish();
-										}
-									}).show();
-					return;
-				case 4:
-					text_news.setText("Could not fetch news from server...");
-					return;
-				}
-
-				Bundle data = msg.getData();
-				String text = data.getString("text");
-				String url = data.getString("url");
-
-				text_news.setText(text);
-
-				Editor prefs = PreferenceManager.getDefaultSharedPreferences(
-						TicketGeneratorActivity.this).edit();
-				prefs.putString("news_text", text);
-				prefs.putString("news_url", url);
-				prefs.putString("news_version",
-						Utils.getVersion(TicketGeneratorActivity.this));
-				prefs.putLong("news_updated", System.currentTimeMillis() / 1000);
-				prefs.commit();
-			}
-		};
-
-		if (text_news != null) {
-			SharedPreferences prefs = PreferenceManager
-					.getDefaultSharedPreferences(TicketGeneratorActivity.this);
-			String text = prefs.getString("news_text", "-");
-
-			if (!text.equals("-")
-					&& prefs.getString("news_version", "0") == Utils
-							.getVersion(this)) {
-				text_news.setText(text);
-				return;
-			}
-
-			Thread t = new Thread() {
-				public void run() {
-					String news = Utils.getNews(TicketGeneratorActivity.this);
-					if (news == null) {
-						handler.sendEmptyMessage(4);
-						return;
-					}
-
-					String[] aStr = news.split("\r");
-					String text = "", url = "";
-					int what;
-
-					if (aStr[0].equals("obsolete")) {
-						if (aStr.length >= 2) {
-							text = aStr[1];
-						}
-						if (aStr.length >= 3) {
-							url = aStr[2];
-						}
-
-						what = 1;
-					} else if (aStr[0].equals("outofdate")) {
-						what = 2;
-					} else {
-						text = aStr[0];
-
-						if (aStr.length >= 2) {
-							url = aStr[1];
-						}
-
-						what = 3;
-					}
-
-					Bundle bundle = new Bundle();
-					bundle.putString("text", text);
-					bundle.putString("url", url);
-					Message msg = new Message();
-					msg.what = what;
-					msg.setData(bundle);
-					msg.setTarget(handler);
-					msg.sendToTarget();
-				}
-			};
-			t.start();
-		}
-	}
-
-	public void click_news(View v) {
-		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		String url = prefs.getString("news_url", "");
-
-		if (url.equals("")) {
-			return;
-		}
-
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		i.setData(Uri.parse(url));
-		startActivity(i);
 	}
 
 	@Override
