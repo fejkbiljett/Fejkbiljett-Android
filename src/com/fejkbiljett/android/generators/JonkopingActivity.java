0ba1efcc61a0f663c3b0e3c0e95a18e7223178d2
Olangu <olangu@impressions.se>
2013-12-07 01:57:05 +0100

    Add initial support for Jonkoping tickets.

---
+++ src/com/fejkbiljett/android/generators/JonkopingActivity.java
@@ -0,0 +1,113 @@
+package com.fejkbiljett.android.generators;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import com.fejkbiljett.android.R;
+import com.fejkbiljett.android.tickets.JonkopingTicket;
+import com.fejkbiljett.android.tickets.Ticket;
+
+public class JonkopingActivity extends TicketGeneratorActivity {
+	public void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.ticket_jonkoping);
+
+		mCityName = "Jönköping";
+
+		super.onCreate(savedInstanceState);
+	}
+
+	public Bundle getParams() {
+		Bundle data = new Bundle();
+
+		boolean bReduced;
+		if (((RadioButton) findViewById(R.id.price_reduced)).isChecked()) {
+			bReduced = true;
+		} else {
+			bReduced = false;
+		}
+
+		/*
+		int iZones = 0;
+		if (((RadioButton) findViewById(R.id.zone_1)).isChecked()) {
+			iZones = 1;
+		} else if (((RadioButton) findViewById(R.id.zone_2)).isChecked()) {
+			iZones = 2;
+		} else if (((RadioButton) findViewById(R.id.zone_3)).isChecked()) {
+			iZones = 3;
+		} else if (((RadioButton) findViewById(R.id.zone_4)).isChecked()) {
+			iZones = 4;
+		} else if (((RadioButton) findViewById(R.id.zone_5)).isChecked()) {
+			iZones = 5;
+		} else if (((RadioButton) findViewById(R.id.zone_6)).isChecked()) {
+			iZones = 6;
+		} else if (((RadioButton) findViewById(R.id.zone_7)).isChecked()) {
+			iZones = 7;
+		}
+		*/
+		
+		data.putBoolean("price_reduced", bReduced);
+		//data.putInt("zones", iZones);
+
+		return data;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+		if (prefs.getBoolean("price_reduced", false)) {
+			((RadioButton) findViewById(R.id.price_reduced)).setChecked(true);
+		}
+
+		/*
+		switch (prefs.getInt("zones", 0)) {
+		case 1:
+			((RadioButton) findViewById(R.id.zone_1)).setChecked(true);
+			break;
+		case 2:
+			((RadioButton) findViewById(R.id.zone_2)).setChecked(true);
+			break;
+		case 3:
+			((RadioButton) findViewById(R.id.zone_3)).setChecked(true);
+			break;
+		case 4:
+			((RadioButton) findViewById(R.id.zone_4)).setChecked(true);
+			break;
+		case 5:
+			((RadioButton) findViewById(R.id.zone_5)).setChecked(true);
+			break;
+		case 6:
+			((RadioButton) findViewById(R.id.zone_6)).setChecked(true);
+			break;
+		case 7:
+			((RadioButton) findViewById(R.id.zone_7)).setChecked(true);
+			break;
+		default:
+			//ERROR
+			break;
+		}
+		*/
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		Editor editor = getPreferences(MODE_PRIVATE).edit();
+		Bundle data = getParams();
+		
+		editor.putBoolean("price_reduced", data.getBoolean("price_reduced"));
+		//editor.putInt("zones", data.getInt("zone"));
+
+		editor.commit();
+	}
+
+	@Override
+	public Ticket getTicket() {
+		return new JonkopingTicket();
+	}
+}
---
+++ src/com/fejkbiljett/android/tickets/JonkopingTicket.java
@@ -0,0 +1,92 @@
+package com.fejkbiljett.android.tickets;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.os.Bundle;
+
+import com.fejkbiljett.android.Utils;
+
+public class JonkopingTicket extends Ticket {
+	private int[] gNumbers = new int[3];
+
+	protected String mSender, uCode = "";
+	
+	
+	protected String mPriceType, mValidDate, mValidTime, mPrice = "";
+
+	protected boolean mReduced;
+	protected int mZones;
+	
+	/*
+	static protected int [] mReducedPrices = { 16, 22, 31,  41, 50, 59, 68 };
+	static protected int [] mPrices = { 26, 37, 52, 67, 83, 98, 114 };
+	
+	static protected double mDiscount = 0.1;
+	*/
+	
+	@Override
+	public String getMessage() {
+		return "" + gNumbers[0] + gNumbers[1] + gNumbers[2] + " JLT\n\n"
+				
+				+ "Giltig till kl " 	+ mValidTime + ", " + mValidDate + "\n"
+				+ mPriceType + " JÖNKÖPING Tätortstrafik\n" 
+				+ mPrice + " SEK (6% MOMS)\n"
+				+ uCode + "\n";
+	}
+	
+
+	@Override
+	public String getSender() {
+		return mSender;
+	}
+
+	@Override
+	public String getMessageOut() {
+		return "J" + (mReduced ? "S" : "V");
+	}
+
+	@Override
+	public String getNumberOut() {
+		return "72344";
+	}
+
+	@Override
+	public void create(Bundle data) throws TicketException {
+		mSender = generateSenderNumber();
+
+		Calendar cal = Calendar.getInstance();
+		Date now = new Date();
+
+		mReduced = data.getBoolean("price_reduced");
+		mZones = data.getInt("zones");
+
+		if (mReduced) {
+			mPriceType = "SKOLUNGDOM";
+			//mPrice = String.format("%.2f", mReducedPrices[mZones-1]*(1-mDiscount));
+			mPrice = "16";
+		} else {
+			mPriceType = "VUXEN";
+			//mPrice = String.format("%.2f", mPrices[mZones-1]*(1-mDiscount));
+			mPrice = "26";
+		}
+
+		cal.add(Calendar.MINUTE, 90);
+		mValidTime = new SimpleDateFormat("HH:mm").format(cal.getTime());
+		mValidDate = new SimpleDateFormat("dd MMM yy").format(cal.getTime()).toLowerCase();
+
+		uCode = Utils.getRandChars("123456789ABCDEFGHJKLMNPQRSTUVWX", 9);
+	}
+
+	private String generateSenderNumber() {
+		String number = "72344";
+
+		for (int i = 0; i < 3; i++) {
+			gNumbers[i] = (int) Math.round(Math.random() * 9);
+			number += gNumbers[i];
+		}
+		
+	return number;
+	}
+}
